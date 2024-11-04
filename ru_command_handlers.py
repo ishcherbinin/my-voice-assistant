5f0a714166bdfb35fe30a9ba9627f584036563ed
Ivan Shcherbinin <ivan.shcherbinin1991@gmail.com>
2024-11-04 19:35:04 +0400

    Resolved problem with import hanlders

--- ru_command_handlers.py
+++ ru_command_handlers.py
@@ -1,21 +1,26 @@
+import logging
 import webbrowser
+from urllib3.util.url import parse_url, Url
+
+_logger = logging.getLogger(__name__)
 
 HANDLERS = []
 
 def command_handler(*args):
+    HANDLERS.append(args[0])
     def wrapper(func):
-        HANDLERS.append(func)
         return func
     return wrapper
 
+
 @command_handler
 def google_search(query):
-    print(query)
     if not "найди" in query:
         return
     try:
-        search_url = f"https://www.google.com/search?q={query}"
+        search_url: Url = parse_url(f"https://www.google.com/search?q={query}")
         # Open the web browser with the search results
-        webbrowser.open(search_url)
+        _logger.info(f"Opening the web browser with the search results for: {search_url}")
+        webbrowser.open(search_url.url)
     except Exception as e:
         print(f"An error occurred: {e}")
